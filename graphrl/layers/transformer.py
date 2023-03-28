540482cde4cc41f1b8b15dae14fd1e1bc0560e1a
jooho <jooho414@gmail.com>
2023-03-28 14:45:37 +0900

    fix the name of argument

--- graphrl/layers/transformer.py
+++ graphrl/layers/transformer.py
@@ -5,14 +5,14 @@ from attention import MultiHeadAttention
 
 
 class TransformerEncoder(nn.Module):
-    def __init__(self, d_model, n_heads, feed_dim, dropout):
+    def __init__(self, model_dim, n_heads, feed_dim, dropout):
         super().__init__()
 
         self.dropout = nn.Dropout(p=dropout)
-        self.attention = MultiHeadAttention(n_heads, d_model, dropout)
-        self.feedforward = PositionWiseFeedForward(d_model, feed_dim, dropout)
-        self.input = AddNorm(d_model, dropout)
-        self.output = AddNorm(d_model, dropout)
+        self.attention = MultiHeadAttention(n_heads, model_dim, dropout)
+        self.feedforward = PositionWiseFeedForward(model_dim, feed_dim, dropout)
+        self.input = AddNorm(model_dim, dropout)
+        self.output = AddNorm(model_dim, dropout)
 
     def forward(self, x, mask):
         x = self.input(x, lambda _x: self.attention(_x, _x, _x), mask=mask)
--- graphrl/utils/layer_utils.py
+++ graphrl/utils/layer_utils.py
@@ -3,13 +3,13 @@ import torch.nn as nn
 
 
 class PositionWiseFeedForward(nn.Module):
-    def __init__(self, d_model, d_ff, dropout, activation=nn.GELU()):
+    def __init__(self, model_dim, feed_dim, dropout, activation=nn.GELU()):
         super().__init__()
 
         self.dropout = nn.Dropout(p=dropout)
 
-        self.linear1 = nn.Linear(d_model, d_ff)
-        self.linear2 = nn.Linear(d_ff, d_model)
+        self.linear1 = nn.Linear(model_dim, feed_dim)
+        self.linear2 = nn.Linear(feed_dim, model_dim)
 
         self.activation = activation
 
@@ -19,10 +19,10 @@ class PositionWiseFeedForward(nn.Module):
 
 
 class AddNorm(nn.Module):
-    def __init__(self, d_model, dropout):
+    def __init__(self, model_dim, dropout):
         super().__init__()
 
-        self.norm = nn.LayerNorm(d_model)
+        self.norm = nn.LayerNorm(model_dim)
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x, layer):
